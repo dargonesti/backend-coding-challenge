3215019895b3734f2c236af005ba9098cff31353
Unknown <migneault62@hotmail.com>
2018-08-20 11:42:26 -0400

    Update test + retour Json
    Ajout de tests avec Supertest et tape. 
    Changement du format de retour pour Json.

--- index.js
+++ index.js
@@ -15,30 +15,30 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.get('/suggestions/', function (req, res) { 
       console.log(req.query);
 
-      res.status(200).send(searchCities(req.query));
-      
+      //res.status(200).send(searchCities(req.query));
+       
+      res.json(searchCities(req.query));
 });
 
-express()
+var app = express()
 .use("/", router) ///suggestions
 .listen(PORT, function(req,res){
   return "ok";
 });
 
   function searchCities(req){
-    return JSON.stringify(
-      {
-      suggestions: cities.map(function(city){
-      //console.log(city);    
+    return {
+        suggestions: cities.map(function(city){   
         return { 
         name: city.name + ", " + city.admin1 + ", "  + city.country,
         latitude: city.lat,
         longitude: city.long,
         score: scores.getCityScore(req, city)
-        //("name" in city ? citySimilarity(nameQuery, city.ascii) : 0)
       };
     })
     .filter(cityComparison=>cityComparison.score > 0)
     .sort((c1,c2) => c2.score - c1.score)
-    }, null, 4);
+    };
   }
+
+  module.exports = app;
--- test.js
+++ test.js
@@ -1,28 +1,31 @@
-const { spawn } = require('child_process');
-const request = require('request');
+//const { spawn } = require('child_process');
+//const request = require('request');
+var request = require('supertest');
 const test = require('tape');
+var app = require("./index.js");
 
+console.log("t0");
 // Start the app
 const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
 
-test('responds to requests', (t) => {
-  t.plan(4);
+test("GET /suggestions?q=Montre", function(assert){
+  request(app)
+  .get("/suggestions?q=Montre")
+  .expect(200)
+  .expect('Content-Type',/json/)
+  .end(function(err,res){
+    var expectedCities = [{"name":"Montréal, 10, CA",
+    "latitude":"45.50884",
+    "longitude":"-73.58781",
+    "score":0.75},
+    {"name":"Montréal-Ouest, 10, CA",
+    "latitude":"45.45286",
+    "longitude":"-73.64918",
+    "score":0.42857142857142855}];
+    var actualCities = res.body;
 
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
-      // stop the server
-      child.kill();
-
-      // No error
-      t.false(error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
-    });
-  });
+    assert.error(err,"No error");
+    assert.same(expectedCities, actualCities.suggestions);
+    assert.end();
+  })
 });
